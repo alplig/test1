3df8139b4f4e04c0be36f8a6c86800f43660d134
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2021-05-12 19:21:36 +0400

    Initial commit

---
+++ src/demo/my/Main.java
@@ -0,0 +1,18 @@
+package demo.my;
+
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User();
+        String Name_eq = "simple";//переменная для сравнения
+        for (int i =0; i <user.Name.size(); i++){
+            if (user.Name.get(i).toLowerCase().contains(Name_eq)) {
+                System.out.println(user.Name.get(i));
+            }
+
+        }
+    }
+}
---
+++ src/demo/my/User.java
@@ -0,0 +1,10 @@
+package demo.my;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class User {
+    List<String> Name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe");
+}
