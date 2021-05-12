6cf899cf9323aa1c17831ad8ee2360ab1c9b2379
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2021-05-12 19:21:36 +0400

    проба комита

--- src/demo/my/User.java
+++ src/demo/my/User.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 
 public class User {
-    List<String> Name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe", "new имя");
+    List<String> Name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe", "new");
 }
