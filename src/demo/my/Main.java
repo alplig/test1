6948a062f9c23498e3c07d5ef877b62aaaa9e177
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2021-08-13 15:21:47 +0400

    setter and getter

--- src/demo/my/Main.java
+++ src/demo/my/Main.java
@@ -1,14 +1,20 @@
 package demo.my;
 
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
       User user = new User();
-        String Name_eq = "simple";//переменная для сравнения
+    //  System.out.println(user.getName_eq());
+      Scanner in = new Scanner(System.in);
+      System.out.print("Введите данные для поиска:");
+      user.setName_eq(in.nextLine());
+     // System.out.println(user.getName_eq());
+
         for (int i =0; i <user.getName().size(); i++){
-            if (user.getName().get(i).toLowerCase().contains(Name_eq)) {
+            if (user.getName().get(i).toLowerCase().contains(user.getName_eq())) {
                 System.out.println(user.getName().get(i));
             }
 
--- src/demo/my/User.java
+++ src/demo/my/User.java
@@ -3,12 +3,26 @@ package demo.my;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 
 public class User {
-     private  List<String> name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe");
+    private final List<String> name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe", "Фильтр1","Фильтр2");
+    private  String name_eq = "simple"; //переменная для сравнения по умолчанию
 
     public List<String> getName() {
         return name;
+    }
+
+    public String getName_eq(){
+        return name_eq;
+    }
+
+    public void setName_eq(String name_eq) {
+        if (!Objects.equals(name_eq, "")){ // было: name_eq!=""
+        this.name_eq = name_eq;
+        } else{
+            System.out.println("Пустой ввод, будет отфильтровано значением по умолчанию \"simple\"");
+        }
     }
 }
