9cdd00dc1cf98de05e22d5a4faf4b000de368123
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2021-08-25 18:42:17 +0400

    стрим вынесен в отдельный метод

--- src/demo/my/Main.java
+++ src/demo/my/Main.java
@@ -1,38 +1,45 @@
 package demo.my;
 
-
-import java.util.Objects;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 
 public class Main {
 
     public static void main(String[] args) {
+
+        searchNameUser();
+        filterUser().forEach(System.out::println);
+
+    }
+
+/*
+    private static void searchNameUser() {
         User user = new User();
-       /* Stream1 a = new Stream1();
-        boolean flag = true;
-        System.out.println(user.getName_eq());*/
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите данные для поиска:");
-        user.setName_eq(in.nextLine());
-
-
-        user.getName().stream().filter(x -> x.toLowerCase().contains(user.getName_eq().toLowerCase())).forEach(System.out::println);
-
-       /* Stream<String> streamName = user.getName().stream();
-        streamName.filter(x -> x == user.getName_eq())
-                .forEach(System.out::println);*/
-
-
-
-       /* for (int i =0; i <user.getName().size(); i++){
+        Boolean flag = true;
+        for (int i = 0; i < user.getName().size(); i++) {
             if (user.getName().get(i).toLowerCase().contains(user.getName_eq())) {
                 System.out.println(user.getName().get(i));
                 flag = false;
             }
         }
         if (flag) {
-            System.out.print("Совпадения значений с: "+"\""+user.getName_eq()+"\""+" не найдены");
-        } */
+            System.out.print("Совпадения значений с: " + "\"" + user.getName_eq() + "\"" + " не найдены");
+        }
+    }*/
+
+    private static void  searchNameUser() {
+        User user = new User();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите данные для поиска:");
+        user.setName_eq(in.nextLine());
+    }
+
+    private static List<String> filterUser() { //почему должен быть обязательно static?
+        User user = new User();
+        return user.getName().stream()
+                .filter(x -> x.contains(user.getName_eq().toLowerCase()))
+                .collect(Collectors.toList());
     }
 }
--- src/demo/my/User.java
+++ src/demo/my/User.java
@@ -4,14 +4,16 @@ package demo.my;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
+
 
 
 public class User {
-    private  List<String> name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe", "Фильтр1","Фильтр2");
+
+    private final List<String> name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe", "Фильтр1","Фильтр2"); //зачем final?
     private  String name_eq = "simple"; //переменная для сравнения по умолчанию
 
     public List<String> getName() {
+        System.out.println("в геттере name_eq="+name_eq);
         return name;
     }
 
@@ -22,6 +24,7 @@ public class User {
     public void setName_eq(String name_eq) {
         if (!Objects.equals(name_eq, "")){ // было: name_eq!=""
         this.name_eq = name_eq;
+        System.out.println("в сеттере name_eq="+this.name_eq);
         } else{
             System.out.println("Пустой ввод, будет отфильтровано значением по умолчанию: \""+this.name_eq+"\"");
         }
