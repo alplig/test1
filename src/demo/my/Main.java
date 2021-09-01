2fd2ea55cf6cb035e45facf62d1397e11475ec9b
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2021-09-01 10:02:27 +0400

    fix учета регистра

--- src/demo/my/Main.java
+++ src/demo/my/Main.java
@@ -8,25 +8,23 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-
         searchNameUser();
         filterUser().forEach(System.out::println);
-
     }
 
 /*
-    private static void searchNameUser() {
-        User user = new User();
-        Boolean flag = true;
-        for (int i = 0; i < user.getName().size(); i++) {
-            if (user.getName().get(i).toLowerCase().contains(user.getName_eq())) {
-                System.out.println(user.getName().get(i));
-                flag = false;
-            }
-        }
-        if (flag) {
-            System.out.print("Совпадения значений с: " + "\"" + user.getName_eq() + "\"" + " не найдены");
-        }
+*    private static void searchNameUser() {
+ *       User user = new User();
+ *       Boolean flag = true;
+  *      for (int i = 0; i < user.getName().size(); i++) {
+  *          if (user.getName().get(i).toLowerCase().contains(user.getName_eq())) {
+    *            System.out.println(user.getName().get(i));
+    *            flag = false;
+     *       }
+     *   }
+      *  if (flag) {
+      *      System.out.print("Совпадения значений с: " + "\"" + user.getName_eq() + "\"" + " не найдены");
+      *  }
     }*/
 
     private static void  searchNameUser() {
@@ -39,7 +37,7 @@ public class Main {
     private static List<String> filterUser() { //почему должен быть обязательно static?
         User user = new User();
         return user.getName().stream()
-                .filter(x -> x.contains(user.getName_eq().toLowerCase()))
+                .filter(x -> x.toLowerCase().contains(user.getName_eq().toLowerCase()))
                 .collect(Collectors.toList());
     }
 }
--- src/demo/my/User.java
+++ src/demo/my/User.java
@@ -10,23 +10,23 @@ import java.util.Objects;
 public class User {
 
     private final List<String> name = Arrays.asList("Имя1","simple Имя2","Имя3 simple","simpleИмя4", "Имя5simple", "ИмЯ6SiMpLe", "Фильтр1","Фильтр2"); //зачем final?
-    private  String name_eq = "simple"; //переменная для сравнения по умолчанию
+    private static String name_eq = "simple"; //переменная для сравнения по умолчанию
 
     public List<String> getName() {
-        System.out.println("в геттере name_eq="+name_eq);
         return name;
     }
 
     public String getName_eq(){
+        //System.out.println("в геттере name_eq="+name_eq);
         return name_eq;
     }
 
     public void setName_eq(String name_eq) {
         if (!Objects.equals(name_eq, "")){ // было: name_eq!=""
-        this.name_eq = name_eq;
-        System.out.println("в сеттере name_eq="+this.name_eq);
+        User.name_eq = name_eq;
+        //System.out.println("в сеттере name_eq="+ this.name_eq);
         } else{
-            System.out.println("Пустой ввод, будет отфильтровано значением по умолчанию: \""+this.name_eq+"\"");
+            System.out.println("Пустой ввод, будет отфильтровано значением по умолчанию: \""+ User.name_eq +"\"");
         }
     }
 }
